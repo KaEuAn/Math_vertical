63a743575e405c70ba4cc220f106965fbd600021
Kailiak Evgeniy <kailiakeugene@mail.ru>
2020-08-01 18:47:55 +0300

    rework, add new class

--- __main__.py
+++ __main__.py
@@ -2,13 +2,11 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
+from collections import Iterable
 
 def column_letter(column_number):
     return chr(ord('A')  - 1 + column_number)
 
-fi = open("sheets.json", "r", encoding='utf-8')
-sheets = json.load(fi)
-fi.close()
 
 class google_tables:
     def __init__(self, docs_name="Рассылки", wks_name="Координаторы", gc_prev=None):
@@ -45,53 +43,105 @@ class google_tables:
                 i += 1
             cur_col += 1
         return cell_table
+    
+    def get_input(self):
+        dic = {}
+        print("insert numbers/names of schools:")
+        for name in input().split(', '):
+            dic[name.upper()] = 0        
+        return dic
 
  
     def get_mails_by_school(self):
         cell_table = self.get_cell_table(columns = (1,3))
-        schools = {}
-        print("insert numbers/names of schools:")
-        for name in input().split(', '):
-            schools[name.upper()] = 0
+        schools = self.get_input()
             
         answer = []
         i = 0
         for i in range(len(cell_table)):
-            if cell_table[i][0].value in schools:
-                schools[cell_table[i][0].value] += 1
+            current_school = cell_table[i][0].value.upper()
+            if current_school in schools:
+                schools[current_school] += 1
                 answer.append(cell_table[i][2].value)
-                if schools[cell_table[i][0].value] > 1:
+                if schools[current_school] > 1:
                     print("more than 1 row for 1 school", cell_table[i][0].value)
         for ans in answer:
             print(ans, end= ', ')
+        print()
         for sch, item in schools.items():
             if item == 0:
                 print("no mail for school", sch)
     
-    def detect_copy(self):
-        cell_table = self.get_cell_table(columns = [1,3])
+    def detect_copy(self, working_columns = [1,3]):
+        cell_table = self.get_cell_table(columns = working_columns)
         mails = {}
         for i in range(len(cell_table)):
             if cell_table[i][0].value != '' and cell_table[i][1].value != '' and cell_table[i][2].value != '':
                 mails_list = cell_table[i][2].value.split(', ')
                 for mail in mails_list:
-                    if mail in mails.keys():
+                    if mail.upper() in mails.keys():
                         print("in row {} detected repeat with row {}, mail {}, sheet {}".format(
-                            i + 1, mails[mail], mail, self.sheet.title))
+                            i + 1, mails[mail.upper()], mail, self.sheet.title))
                     else:
-                        mails[mail] = i + 1
+                        mails[mail.upper()] = i + 1
+                        
+    def detect_if_in_table(self):
+        new_mails = self.get_input()
+        cell_table = self.get_cell_table(columns = [1,3])
+        mails = set()
+        for i in range(len(cell_table)):
+            if cell_table[i][0].value != '' and cell_table[i][1].value != '' and cell_table[i][2].value != '':
+                mails_list = cell_table[i][2].value.split(', ')
+                for mail in mails_list:
+                    mails.add(mail)
+        for new_mail in new_mails:
+            print(new_mail in mails, new_mail)
+            
 
-def get_mails_by_school():
-    gt = google_tables(wks_name="участники семинаров")
-    gt.get_mails_by_school()
+class MyTables:
 
-def check_copies():
-    gc = None
-    for sheet in sheets:
-        gt = google_tables(gc_prev = gc, wks_name=sheet)
-        gt.detect_copy()
-        gc = gt.gc
-
+    def __init__(self, sheets=None, doc_name=None):
+        if sheets is None:
+            fi = open("sheets.json", "r", encoding='utf-8')
+            self.sheets = json.load(fi)
+            fi.close()
+        else:
+            self.sheets = sheets
+            
+        if doc_name is None:
+            self.doc_name = "Рассылки"
+        else:
+            self.doc_name = doc_name
+            
+    
+    def check_copies_summer(self):
+        pass
+            
+    
+    def get_mails_by_school(self, name="Координаторы"):
+        gt = google_tables(docs_name = self.doc_name, wks_name=name)
+        gt.get_mails_by_school()
+        
+    def do_for_all_sheets(self, function, *args, **kwargs):
+        gc = None
+        for sheet in self.sheets:
+            gt = google_tables(docs_name = self.doc_name, gc_prev = gc, wks_name=sheet)
+            function(gt, *args, **kwargs)
+            gc = gt.gc    
+    
+    def check_copies(self):
+        return self.do_for_all_sheets(google_tables.detect_copy)
+            
+    def check_detection(self):
+        gt = google_tables(wks_name="участники семинаров")
+        gt.detect_if_in_table()
+        
+    def check_kruzki(self):
+        gt = google_tables(wks_name='Кружки')
+        gt.detect_copy([3,5])
+    
 if __name__ == "__main__":
-    #get_mails_by_school()
-    check_copies()
+    table = MyTables(["Лист1"], "Ведомость")
+    #check_kruzki()
+    table.check_copies()
+    #check_copies()
